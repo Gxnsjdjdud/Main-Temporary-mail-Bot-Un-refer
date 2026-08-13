ad6e0feff368b1470ca56f13a80bb194199dd634
Gxnsjdjdud <fxyoutubenoid@gmail.com>
2026-08-13 13:31:31 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ from database import (
 # SETTINGS
 # ============================================================
 
-API_BASE = "https://smails.dev/api"
+API_BASE = "https://tempmailc.com/api/v1"
 
 POLL_SECONDS = 3
 
