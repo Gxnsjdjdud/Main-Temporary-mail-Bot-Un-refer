0fd7d6a0020b038ce6c29818bbbdee37eb7f9cbf
Gxnsjdjdud <fxyoutubenoid@gmail.com>
2026-08-15 17:57:48 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
-# config.py
+import os
 
-# Telegram BotFather থেকে পাওয়া Bot Token
-BOT_TOKEN = "8712260735:AAFIzKrfh2iSS47vEe_uLKWpNc7MsT7Py7g"
-
-# Admin Telegram User ID
-# একাধিক admin হলে এভাবে লিখবে:
-# ADMIN_IDS = [8136997138, 8136997138]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 ADMIN_IDS = [
     8136997138
